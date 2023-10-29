29886602199e629fcbf5faaea25d3bcf4dfc1025
DanielGomez <killergomez777@gmail.com>
2023-10-29 18:36:59 -0500

    continuaion

---
+++ superMarket-mvp/Models/ICategoryRepository.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Supermarker_mvp.Models
+{
+    internal interface ICategoryRepository
+    {
+        void Add(CategoryModel categoriesModel);
+        void Edit(CategoryModel categoriesModel);
+        void Delete(int id);
+        IEnumerable<CategoryModel> GetAll();
+        IEnumerable<CategoryModel> GetByValue(string values);
+    }
+}
