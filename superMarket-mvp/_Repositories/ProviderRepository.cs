ee8fa69950149fa519886f87af60210d16e9d1d9
DanielGomez <killergomez777@gmail.com>
2023-10-29 19:12:15 -0500

    finished

--- superMarket-mvp/_Repositories/ProviderRepository.cs
+++ superMarket-mvp/_Repositories/ProviderRepository.cs
@@ -1,8 +1,9 @@
-﻿using Microsoft.Data.SqlClient;
+﻿
 using Supermarker_mvp.Models;
 using System;
 using System.Collections.Generic;
 using System.Data;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
